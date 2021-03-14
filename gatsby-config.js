894b6622759a80643602d2351caa96f69f38bcc0
Tiger Taylor <tigertaylor1024@gmail.com>
2021-03-14 22:39:57 +0000

    Update dependencies, name, and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,20 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Tiger Taylor",
+    titleTemplate: "%s · ${site.siteMetadata.title}",
+    description: "Wesbite for Tiger Taylor (actuallyatiger)",
+    url: "https://actuallyatiger.netlify.app",
+    image: "/images/logo.jpg", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@actuallyatiger",
+  },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Tiger Taylor - Portfolio`,
+        name: `Tiger Taylor · Wesbite`,
         short_name: `Tiger Taylor`,
         lang: `en`,
         display: `standalone`,
