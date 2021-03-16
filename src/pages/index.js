96a7e5fc4d7cf2f4f5ddeb0908df68ac6e056b09
Tiger Taylor <tigertaylor1024@gmail.com>
2021-03-16 22:34:34 +0000

    Add basic homepage

---
+++ src/pages/index.js
@@ -0,0 +1,79 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const Home = () => {
+  return (
+    <Layout title="Home">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed
+        vulputate mi. Est sit amet facilisis magna etiam. Euismod in
+        pellentesque massa placerat duis ultricies lacus. Mattis nunc sed
+        blandit libero volutpat sed cras ornare. Arcu cursus vitae congue mauris
+        rhoncus aenean vel elit scelerisque. Condimentum mattis pellentesque id
+        nibh tortor. Cras adipiscing enim eu turpis egestas pretium aenean
+        pharetra. Fermentum dui faucibus in ornare quam viverra orci sagittis
+        eu. Augue eget arcu dictum varius duis at consectetur.
+      </p>
+      <br />
+      <p>
+        In nulla posuere sollicitudin aliquam ultrices sagittis orci. Pulvinar
+        sapien et ligula ullamcorper malesuada proin. Condimentum id venenatis a
+        condimentum vitae. Amet consectetur adipiscing elit duis tristique
+        sollicitudin nibh. Dignissim sodales ut eu sem. Non consectetur a erat
+        nam at. Nullam vehicula ipsum a arcu cursus vitae congue mauris.
+        Lobortis scelerisque fermentum dui faucibus in ornare. Pulvinar
+        pellentesque habitant morbi tristique senectus et netus. Malesuada fames
+        ac turpis egestas integer eget. In iaculis nunc sed augue lacus viverra
+        vitae. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper
+        velit.
+      </p>
+      <br />
+      <p>
+        Justo nec ultrices dui sapien eget mi. Orci eu lobortis elementum nibh.
+        Mauris augue neque gravida in fermentum et sollicitudin ac. Tincidunt
+        tortor aliquam nulla facilisi cras fermentum odio eu. Malesuada proin
+        libero nunc consequat interdum varius. Est pellentesque elit ullamcorper
+        dignissim cras tincidunt lobortis feugiat. Tempus imperdiet nulla
+        malesuada pellentesque elit eget. Adipiscing elit ut aliquam purus sit
+        amet luctus venenatis lectus. Lacus vestibulum sed arcu non odio. Tellus
+        integer feugiat scelerisque varius morbi enim nunc faucibus. Id velit ut
+        tortor pretium. Aliquet lectus proin nibh nisl condimentum id.
+      </p>
+      <br />
+      <p>
+        Leo vel orci porta non pulvinar. Nec sagittis aliquam malesuada bibendum
+        arcu vitae. Vestibulum lorem sed risus ultricies tristique nulla aliquet
+        enim. Pellentesque elit ullamcorper dignissim cras tincidunt. Enim
+        facilisis gravida neque convallis a cras semper auctor. Ipsum faucibus
+        vitae aliquet nec ullamcorper sit. Sed sed risus pretium quam vulputate
+        dignissim suspendisse in est. Nibh venenatis cras sed felis eget velit.
+        Massa vitae tortor condimentum lacinia quis vel eros donec. Amet purus
+        gravida quis blandit turpis cursus in. Mauris augue neque gravida in
+        fermentum et sollicitudin. Nulla pharetra diam sit amet nisl. Egestas
+        purus viverra accumsan in.
+      </p>
+      <br />
+      <p>
+        Et tortor at risus viverra adipiscing. Dignissim suspendisse in est ante
+        in nibh mauris cursus mattis. Vitae turpis massa sed elementum. Vitae
+        elementum curabitur vitae nunc sed velit dignissim. Viverra ipsum nunc
+        aliquet bibendum enim facilisis. Aliquet nec ullamcorper sit amet.
+        Habitant morbi tristique senectus et netus et malesuada. Mauris vitae
+        ultricies leo integer malesuada nunc vel. Eros in cursus turpis massa
+        tincidunt dui ut. Morbi tristique senectus et netus et. Pretium aenean
+        pharetra magna ac placerat vestibulum. Molestie nunc non blandit massa
+        enim nec. Sit amet tellus cras adipiscing enim eu turpis egestas. Urna
+        et pharetra pharetra massa massa ultricies mi quis. Viverra mauris in
+        aliquam sem fringilla ut morbi tincidunt augue. Tincidunt eget nullam
+        non nisi est sit amet. Euismod nisi porta lorem mollis aliquam ut.
+        Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed. Sit
+        amet luctus venenatis lectus magna fringilla. Sagittis orci a
+        scelerisque purus semper.
+      </p>
+    </Layout>
+  )
+}
+
+export default Home
