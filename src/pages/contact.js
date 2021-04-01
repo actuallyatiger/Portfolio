8cd458671224300e85da6301cf0575a324b7f71f
Tiger Taylor <tigertaylor1024@gmail.com>
2021-04-01 23:21:44 +0100

    Added linked page boilerplates

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const Contact = () => {
+  return (
+    <Layout title="Contact"></Layout>
+    )
+}
+
+export default Contact
+
